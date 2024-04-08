52f978a26a82362fd09db615ae01bcc999da6b42
Sonia Aguirre <arembiam@gmail.com>
2024-04-08 10:00:08 -0600

    Login tailwind css format done

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,30 +1,31 @@
 import { useForm } from "react-hook-form";
 import { FaApple, FaGithub, FaTwitter, FaSquare } from "react-icons/fa";
+
 export default function LogIn() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
 
   return (
     <main className="flex p-10 justify-center items-center">
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center">
         {/* TITULO DEV  */}
         <img
           src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/original_logo_0DliJcfsTcciZen38gX9.png"
           alt=""
-          className="h-12 w-auto"
+          className="h-11 w-auto"
         />
-        <div className="flex-col p-6">
+        <div className="flex-col p-4">
           <h1 className="text-center text-3xl font-sans font-bold">
             Join the DEV Community
           </h1>
-          <p className="text-center font-sans text-sm text-gray-700">
+          <p className="text-center font-sans text-md text-gray-700">
             DEV Community is a community of 1,391,112 amazing developers
           </p>
         </div>
 
         {/* LOGIN CON  OTRAS CUENTAS */}
         <div className="flex flex-col gap-3 w-96 justify-center">
-          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-50">
+          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-100">
             <span className="text-xl">
               <FaApple />
             </span>
@@ -32,7 +33,7 @@ export default function LogIn() {
               Continue with Apple
             </span>
           </button>
-          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-50">
+          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-100">
             <span className="text-xl">
               <FaSquare />
             </span>
@@ -40,7 +41,7 @@ export default function LogIn() {
               Continue with Forem
             </span>
           </button>
-          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-50">
+          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-100">
             <span className="text-xl">
               <FaGithub />
             </span>
@@ -48,7 +49,7 @@ export default function LogIn() {
               Continue with GitHub
             </span>
           </button>
-          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-50">
+          <button className="border border-gray-300 rounded-md p-2 inline-flex hover:bg-gray-100">
             <span className="text-xl text-cyan-500">
               <FaTwitter />
             </span>
@@ -59,12 +60,12 @@ export default function LogIn() {
         </div>
 
         {/* DIVISION OR */}
-        <div>
+        <div className="w-96 p-6">
           <p className="text-center font-sans text-sm text-gray-700">OR</p>
         </div>
 
         {/* FORMULARIO CORREO */}
-        <div className="flex flex-col gap-3 w-96 justify-center">
+        <div className="flex flex-col w-96 justify-center">
           <form onSubmit={handleSubmit(onSubmit)}>
             <p className="text-left text-sm font-sans font-semibold py-2">
               Email
@@ -85,9 +86,8 @@ export default function LogIn() {
               required
               class="border border-gray-300 rounded-md w-96 p-1 focus:outline-none focus:border-blue-800 focus:ring-1 focus:ring-blue-700"
             />
-            <div>
-              <div className="flex p-2 w-96 justify-center">
-                <input type="checkbox" name="remberMe" id="" />
+            <div className="flex flex-col justify-center">
+              <div className="flex py-2">
                 <span className="font-sans text-sm text-gray-700 w-60">
                   Remember me
                 </span>
@@ -95,19 +95,49 @@ export default function LogIn() {
                   Forgot password?
                 </span>
               </div>
-              <button type="submit">Log in</button>
+              <button
+                type="submit"
+                className="flex w-96 p-2 justify-center bg-blue-700 rounded-md text-white font-sans font-semibold text-sm
+                "
+              >
+                Log in
+              </button>
             </div>
           </form>
         </div>
-        <div>
-          <p className="text-center">
-            By signing up, you are agreeing to our privacy policy, terms of use
-            and code of conduct.
-          </p>
-          <div>
-            <p className="text-center">New to DEV Community? Create account.</p>
+
+        {/* FOOTER LEGALES */}
+        <div className="flex flex-col p-8 ">
+          <div className="pb-3">
+            <p className="text-center font-sans italic text-sm text-gray-500">
+              By signing up, you are agreeing to our{" "}
+              <a href="" className="text-blue-700">
+                privacy policy
+              </a>
+              ,{" "}
+              <a href="" className="text-blue-700">
+                terms of use
+              </a>
+              <br />
+              and{" "}
+              <a href="" className="text-blue-700">
+                code of conduct
+              </a>
+              .
+            </p>
+          </div>
+
+          <div className="border-t border-gray-300 py-4">
+            <p className="text-center font-sans text-sm text-gray-700">
+              New to DEV Community?{" "}
+              <a href="" className="text-blue-700">
+                Create account.
+              </a>
+            </p>
           </div>
         </div>
+
+        {/* END OF PAGE */}
       </div>
     </main>
   );
