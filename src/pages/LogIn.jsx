e45459628cb106b2083c4f04fc2b2e6fb610cac0
Sonia Aguirre <arembiam@gmail.com>
2024-04-07 23:43:08 -0600

    Login structure ready, made with react hook form

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,25 +1,50 @@
+import { useForm } from "react-hook-form";
 export default function LogIn() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <main className="flex py-8 px-96 justify-center items-center">
       <div className="flex flex-col">
         <div>
           <h1 className="text-center">Join the DEV Community</h1>
           <p className="text-center">
-            DEV Community is a community of 1,232,991 amazing developers
+            DEV Community is a community of 1,391,112 amazing developers
           </p>
         </div>
-        <button>Sign up with Apple</button>
-        <button>Sign up with Forem</button>
-        <button>Sign up with GitHub</button>
-        <button>Sign up with X</button>
-        <button>Sign up with Email</button>
+        <button>Continue with with Apple</button>
+        <button>Continue with with Forem</button>
+        <button>Continue with with GitHub</button>
+        <button>Continue with with Twitter</button>
+        <div>
+          <p>OR</p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <p>Email</p>
+            <input type="text" required {...register("email")} />
+            <p>Password</p>
+            <input
+              type="password"
+              required
+              maxLength={8}
+              {...register("password")}
+            />
+            <div>
+              <div>
+                <input type="checkbox" name="remberMe" id="" />
+                <span>Remember me</span>
+                <span>Forgot password?</span>
+              </div>
+              S<button type="submit">Log in</button>
+            </div>
+          </form>
+        </div>
         <div>
           <p className="text-center">
             By signing up, you are agreeing to our privacy policy, terms of use
-            and code of conduct
+            and code of conduct.
           </p>
           <div>
-            <p className="text-center">Already have an account? Log in.</p>
+            <p className="text-center">New to DEV Community? Create account.</p>
           </div>
         </div>
       </div>
--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,1 +1,28 @@
-export default function NewUser() {}
+export default function NewUser() {
+  return (
+    <main className="flex py-8 px-96 justify-center items-center">
+      <div className="flex flex-col">
+        <div>
+          <h1 className="text-center">Join the DEV Community</h1>
+          <p className="text-center">
+            DEV Community is a community of 1,391,112 amazing developers
+          </p>
+        </div>
+        <button>Sign up with Apple</button>
+        <button>Sign up with Forem</button>
+        <button>Sign up with GitHub</button>
+        <button>Sign up with Twitter</button>
+        <button>Sign up with Email</button>
+        <div>
+          <p className="text-center">
+            By signing up, you are agreeing to our privacy policy, terms of use
+            and code of conduct.
+          </p>
+          <div>
+            <p className="text-center">Already have an account? Log in.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
