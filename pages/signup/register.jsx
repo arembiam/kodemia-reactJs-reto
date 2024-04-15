f760dec4ee877932f13facd72fc044f803469597
Sonia Aguirre <arembiam@gmail.com>
2024-04-14 19:54:12 -0600

    SignUp Register form structure done

--- pages/signup/register.jsx
+++ pages/signup/register.jsx
@@ -1,7 +1,32 @@
+import { useForm } from "react-hook-form";
+
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <main>
-      <h1>Register form here</h1>
+      <div>
+        <h1>Create your account</h1>
+        <form onSubmit={handleSubmit()}>
+          <h2>Profile image</h2>
+          <input type="url" name="profile-image" id="" />
+          <h2>Name*</h2>
+          <input type="text" />
+          <h2>Username*</h2>
+          <input type="text" />
+          <h2>Email*</h2>
+          <input type="email" name="" id="" />
+          <h2>Password*</h2>
+          <input type="password" name="" id="" />
+          <h2>Password Confirmation*</h2>
+          <input type="password" name="" id="" />
+        </form>
+        <button type="submit">Sign up</button>
+      </div>
     </main>
   );
 }
