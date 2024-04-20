1d10b3e1259a60da83eb7b1bfcbebf2cab1a213f
Sonia Aguirre <arembiam@gmail.com>
2024-04-20 12:19:08 -0600

    Post Detail layout and responsive done. Functionality missing.

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,6 +1,6 @@
 import NavBar from "@/src/components/NavBar";
 import FooterPage from "@/src/components/Footer";
-import Link from "next/link";
+
 import { TbHeartPlus } from "react-icons/tb";
 import { FaRegComment, FaRegBookmark } from "react-icons/fa";
 
@@ -13,19 +13,19 @@ export default function Posts() {
         {/* ASIDE IZQUIERDO REACCIONES*/}
         <aside className=" flex flex-col gap-6 w-20 max-lg:visible  max-md:collapse ">
           <div className=" flex flex-col justify-items-center">
-            <span className="text-2xl text-gray-900">
+            <span className="text-2xl text-gray-900 hover:text-rose-500">
               <TbHeartPlus />
             </span>
             <p className="text-sm text-gray-900">15</p>
           </div>
           <div>
-            <span className="text-2xl text-gray-900">
+            <span className="text-2xl text-gray-900 hover:text-amber-500">
               <FaRegComment />
             </span>
             <p className="text-sm text-gray-900">38</p>
           </div>
           <div>
-            <span className="text-2xl text-gray-900">
+            <span className="text-2xl text-gray-900 hover:text-blue-700">
               <FaRegBookmark />
             </span>
             <p className="text-sm text-gray-900">0</p>
@@ -166,21 +166,56 @@ export default function Posts() {
 
         {/* ASIDE DERECHO SOBRE EL AUTOR ESTO ES UN COMPONENT */}
         <aside className="flex flex-col w-96 justify-between max-lg:visible  max-md:collapse">
-          <div className="bg-white rounded-md border border-gray-300 flex flex-col p-4 ">
+          <div className="bg-white rounded-md border border-gray-300 flex flex-col pb-4 ">
             {/* AUTOR DEL POST */}
-            <div className="flex-row inline-flex gap-2 items-center p-2">
-              <div className="inline-flex">
-                <img
-                  src="https://www.nasa.gov/wp-content/uploads/2023/03/pillars_of_creation.jpg"
-                  alt=""
-                  className="h-10 w-auto rounded-full"
-                />
+            <div className="pb-8">
+              <div className=" bg-teal-500 w-full rounded-t-md h-5">
+                <div className="flex-row inline-flex gap-2 items-center py-2">
+                  <div className="inline-flex px-4">
+                    <img
+                      src="https://www.nasa.gov/wp-content/uploads/2023/03/pillars_of_creation.jpg"
+                      alt=""
+                      className="h-10 w-auto rounded-full"
+                    />
+                  </div>
+                  <div className="inline-flex">
+                    <h4 className="text-gray-950 text-sm font-bold hover:text-blue-700">
+                      Sonia Aguirre
+                    </h4>
+                  </div>
+                </div>
               </div>
+            </div>
 
-              <div className="inline-flex">
-                <h4 className="text-gray-950 text-sm font-bold hover:text-blue-700">
-                  Sonia Aguirre
-                </h4>
+            {/* FOLLOW BUTTON */}
+            <div className="px-4 py-2">
+              <button
+                type="button"
+                className="bg-blue-700 text-white text-sm font-medium p-1 w-full rounded-md hover:bg-blue-800"
+              >
+                Follow
+              </button>
+            </div>
+
+            {/*BIO */}
+            <div className="px-4">
+              <div className="py-2">
+                <p className="text-sm text-gray-900">
+                  Graphic Designer & Aspiring Full-Stack Developer. I do stuff
+                  sometimes.
+                </p>
+              </div>
+              <div className="py-1">
+                <p className="text-xs text-gray-600 font-bold">LOCATION</p>
+                <p className="text-sm text-gray-900">Monterrey,MX</p>
+              </div>
+              <div className="py-1">
+                <p className="text-xs text-gray-600 font-bold">WORK</p>
+                <p className="text-sm text-gray-900">Full-stack dev</p>
+              </div>
+              <div className="py-1">
+                <p className="text-xs text-gray-600 font-bold">JOINED</p>
+                <p className="text-sm text-gray-900">May 8, 2023</p>
               </div>
             </div>
           </div>
